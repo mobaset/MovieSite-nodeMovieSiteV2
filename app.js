1862aa5ee36f44e331e8ae15d789f6e8c81c5faa
Mo <mo.baset>
2016-04-06 22:16:43 +0400

    few changes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express = require('express'), fs = require('fs');
 var app = express();
 var port = process.env.PORT || 8080; // a variable that stores a dynamic port
 const http = require('http');
-var apiKey = "api_key= f88a673179c88a480ebd3bfd5b852a57";
+var apiKey = "f88a673179c88a480ebd3bfd5b852a57";
 
 // a middleware to grab CSS and JS files
 app.use('/public', express.static(__dirname + '/Public'));
